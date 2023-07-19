7ca3e13241a0fb43ddd58c72f2602fa5884c4bd2
kkh27373 <kkh27373@naver.com>
2023-07-19 21:28:35 +0900

    ㅇㄴ

--- project/src/main/java/web/project/domain/User.java
+++ project/src/main/java/web/project/domain/User.java
@@ -1,15 +1,9 @@
 package web.project.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class User {
-	private int usercode;
-	private int username;
+	private String username;
+	private String userid;
 }
