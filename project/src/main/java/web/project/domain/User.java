54c32dbb7350276c4a5a8caaf9c645d964b2b9c6
JiyunNam0 <skawldbs2083@naver.com>
2023-07-24 20:18:24 +0900

    fffeeeerrr

--- project/src/main/java/web/project/domain/User.java
+++ project/src/main/java/web/project/domain/User.java
@@ -10,22 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-<<<<<<< HEAD
 	private String username;
 	private String userid;
 	private String category;
 	private String phone;
 	private String email;
 	private String details;
-=======
-	private int user_code;
-	private String user_name;
-	private String user_id;
-	private String user_address;
-	private String user_phone;
-	private int user_gender;
-	private String user_password;
-	private String user_profile_img;
-	private String user_email;
->>>>>>> a0c7763d90be96d8aa0a37dae93b4fb4117ed18d
 }
