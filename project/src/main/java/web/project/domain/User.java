2c9a3a5829f4102d3cd110f5f26b13775be5910f
Mingeun9 <rnalsrms0908@naver.com>
2023-07-24 19:00:26 +0900

    0724

--- project/src/main/java/web/project/domain/User.java
+++ project/src/main/java/web/project/domain/User.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class User {
 	private String username;
 	private String userid;
+	private String category;
+	private String phone;
+	private String email;
+	private String details;
 }
